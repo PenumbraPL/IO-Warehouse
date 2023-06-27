180b53fe59636732c2b1cdfa1591d088da29e248
CoreNest <ernestk@student.agh.edu.pl>
2023-06-27 14:00:49 +0200

    update forms

--- FrontEnd/src/pages/form.js
+++ FrontEnd/src/pages/form.js
@@ -7,40 +7,18 @@ import { PaletasTable, generateRaport } from 'src/sections/customer/paletas-tabl
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
+import useFetch from 'react-fetch-hook';
+import Alert from '@mui/material/Alert';
+
 
 let Sectors = [
   { ID: 'A', racks: [1, 12, 123] },
   { ID: 'B', racks: [1, 2, 3] },
   { ID: 'C', racks: [3, 33, 333] }
 ]
-async function getSectorsData() {
-  try {
-    const user = JSON.parse(localStorage.user)
-    const resp = await fetch('http://localhost:3001/api/sectors', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          //authorization: user.authorization
-        },
-    })
-    if(resp.status != 200){
-      console.log(resp)
-    }
-    return await resp.json()
-  } catch (err) {
-    console.log(err)
-  }
-}
 
-const Page = () => {
 
-  useEffect(() => {
-    const fun = async () => {
-      Sectors = await getSectorsData();
-      console.log(Sectors)
-    }
-    fun();
-  });
+export function GetSectorsData() {
 
   const [selectedSector, setSelectedSector] = useState('All');
   const [selectedRack, setSelectedRack] = useState(-1);
@@ -60,11 +38,11 @@ const Page = () => {
   };
 
 
-  const SectorsListId = (datas) => {
+  let SectorsListId = (datas) => {
     return datas.map(data => (<MenuItem key={data.ID}
       value={data.ID}>{data.ID}</MenuItem>))
   }
-  const SectorsListRack = (datas, val) => {
+  let SectorsListRack = (datas, val) => {
 
     return datas.map(data => (
       (data.ID == val) ?
@@ -74,92 +52,110 @@ const Page = () => {
         <></>
     ))
   }
+  const { isLoading, data, error } = useFetch('http://localhost:3001/api/sectors');
+  if (error) {
 
+    return (<><Alert severity="error">error kod {error.status}: {error.statusText}</Alert>
+      <Alert severity="info">no data 'http://localhost:3001/api/sectors'</Alert></>)
+
+  }
+
+  console.log(data)
+
+  if (!isLoading) {
+    return (<>
+      <Head>
+      <title>
+        Raport
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="xl">
+        <Stack spacing={3}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            spacing={4}
+          >
+            <Stack spacing={1}>
+              <Typography variant="h4">
+                Magazyn
+              </Typography>
+              <Stack
+                alignItems="center"
+                direction="row"
+                spacing={1}
+              >
+                <InputLabel id="select-sector-label">Section</InputLabel>
+                <FormControl sx={{ m: 1, minWidth: 140 }}>
+
+
+                  <Select
+                    labelId="select-sector-label"
+                    id="select-sector"
+                    value={selectedSector}
+                    label="Section"
+                    onChange={handleChangeSector}
+                  >
+                    {/* <option aria-label="All" value="All" label='All' /> */}
+                    {SectorsListId(Sectors)}
+
+                  </Select>
+
+                </FormControl >
+                <InputLabel id="select-rack-label">Rack</InputLabel>
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <Select
+                    labelId="select-rack-label"
+                    id="select-rack"
+                    value={selectedRack}
+                    label="Rack"
+                    onChange={handleChangeRack}
+                  >
+                    {SectorsListRack(Sectors, selectedSector)}
+
+                  </Select>
+                </FormControl>
+              </Stack>
+            </Stack>
+            <div>
+              <Button
+                onClick={generateRaport}
+                startIcon={(
+                  <SvgIcon fontSize="small">
+                    <Cog6ToothIcon class="h-6 w-6 text-gray-500" />
+                  </SvgIcon>
+                )}
+                variant="contained"
+              >
+                Generate raports
+              </Button>
+            </div>
+          </Stack>
+
+
+          {RaportTable}
+
+        </Stack>
+      </Container>
+    </Box></>
+    )
+  }
+}
+
+const Page = () => {
+
+
+  const dat = GetSectorsData();
   return (
     <>
-      <Head>
-        <title>
-          Raport
-        </title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8
-        }}
-      >
-        <Container maxWidth="xl">
-          <Stack spacing={3}>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              spacing={4}
-            >
-              <Stack spacing={1}>
-                <Typography variant="h4">
-                  Magazyn
-                </Typography>
-                <Stack
-                  alignItems="center"
-                  direction="row"
-                  spacing={1}
-                >
-                  <InputLabel id="select-sector-label">Section</InputLabel>
-                  <FormControl sx={{ m: 1, minWidth: 140 }}>
-
-
-                    <Select
-                      labelId="select-sector-label"
-                      id="select-sector"
-                      value={selectedSector}
-                      label="Section"
-                      onChange={handleChangeSector}
-                    >
-                      {/* <option aria-label="All" value="All" label='All' /> */}
-                      {SectorsListId(Sectors)}
-
-                    </Select>
-
-                  </FormControl >
-                  <InputLabel id="select-rack-label">Rack</InputLabel>
-                  <FormControl sx={{ m: 1, minWidth: 120 }}>
-                    <Select
-                      labelId="select-rack-label"
-                      id="select-rack"
-                      value={selectedRack}
-                      label="Rack"
-                      onChange={handleChangeRack}
-                    >
-                      {SectorsListRack(Sectors, selectedSector)}
-
-                    </Select>
-                  </FormControl>
-                </Stack>
-              </Stack>
-              <div>
-                <Button
-                  onClick={generateRaport}
-                  startIcon={(
-                    <SvgIcon fontSize="small">
-                      <Cog6ToothIcon class="h-6 w-6 text-gray-500" />
-                    </SvgIcon>
-                  )}
-                  variant="contained"
-                >
-                  Generate raports
-                </Button>
-              </div>
-            </Stack>
-
-            {/* <CustomersSearch /> */}
-            {RaportTable}
-            {/* <PaletasTable sector={selectedSector}
-              rackID={selectedRack}
-            /> */}
-          </Stack>
-        </Container>
-      </Box>
+{dat}
     </>
   );
 };
